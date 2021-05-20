0a6339fbba713a64b8b30a80b933280c732f84e4
Axel Raboit <axel.raboit@gmail.com>
2021-05-20 20:59:10 +0200

    fix to deploy

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header/Header';
-import $ from "jquery";
 import './HomePage.css';
 import imageMovieProject from '../assets/images/card/movie-project.jpg';
 
